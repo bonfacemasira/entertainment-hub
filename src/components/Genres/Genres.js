74bcf1fb32d5648122f170085e1ea5c0d1c9909e
Bonface Masira <bonfacemasira@gmail.com>
2022-07-26 21:31:03 +0300

    create components for the whole project

---
+++ src/components/Genres/Genres.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './genres.css';
+
+function Genres() {
+    
+}
+
+export default Genres;
---
+++ src/components/PageContent/PageContent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PageContent.css';
+
+function PageContent() {
+    
+}
+
+export default PageContent;
