86f52f8e871849577a989e3eab3e27014b6c78ab
Bonface Masira <bonfacemasira@gmail.com>
2022-07-28 09:27:26 +0300

    finalie on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './Header.css';
+import { FaFilm, BiMoviePlay } from "react-icons/fa"
+
 
 function Header() {
-    
+
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }
+    return(
+        <div className="header" onClick={scrollToTop}>
+            <div className="title">
+            🎬 Entertainment Hub 🎥
+            </div>
+        </div>
+    )
 }
 
 export default Header;
