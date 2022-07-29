ab3c45266d7d23904f7983f3285167cb8ee2cc5f
Bonface Masira <bonfacemasira@gmail.com>
2022-07-29 04:07:05 +0300

    identify the data to be displayed on a single container

---
+++ src/components/SingleContent/SingleContent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function SingleContent({id, poster, title, date, media_type, rating}) {
+    let source = `https://image.tmdb.org/t/p/w1280${poster}`
+    return (
+        <div className="single-content">
+            <div className="image-container">
+                <img className="image" src={source} alt={title} />
+            </div>
+            <div className="description-container">
+                <p>{media_type}</p>
+                <h2>{title}</h2>
+                <p>{date}</p>
+                <p>{rating}</p>
+            </div>
+        </div>
+    )
+}
+
+export default SingleContent;
