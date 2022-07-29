01016d227800d7182f221f66bd5ef40a4f400821
Bonface Masira <bonfacemasira@gmail.com>
2022-07-30 02:34:03 +0300

    reduce the width of container

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -6,11 +6,12 @@ function SingleContent({id, poster, title, date, media_type, rating}) {
     let source = `https://image.tmdb.org/t/p/w1280${poster}`
     return (
         <div className="single-content">
-            <div className="image-container">
+            <img className="image" src={source} alt={title} />
+            {/* <div className="image-container">
                 <img className="image" src={source} alt={title} />
-            </div>
+            </div> */}
             <div className="description-container">
-                <h3 className="title">{title}</h3>
+                <p className="title">{title}</p>
                 <div className="holder">
                     <span>{media_type === "movie" ? "Movie" : "TV Series"}</span>
                     <span>{date}</span>
