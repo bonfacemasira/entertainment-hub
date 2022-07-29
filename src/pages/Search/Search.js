f235e75fecd2c046ad53443d442eb7ce43078819
Bonface Masira <bonfacemasira@gmail.com>
2022-07-30 01:41:53 +0300

    add the search bar  to the search route

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Search.css';
+import SingleContent from '../../components/SingleContent/SingleContent';
 
 function Search() {
+    const [searchQuery, setSearchQuery] = useState('');
+
     return(
-        <div className="search"></div>
+        <div className="search">
+            <form className="search">
+                <input type="text" className="search-input" placeholder="Search..." />
+                <button>Search</button>
+            </form>
+        </div>
     )
 }
 
