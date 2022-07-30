8c8c1abc49f044699308268fe02eb9c8324e5c3f
Bonface Masira <bonfacemasira@gmail.com>
2022-07-30 23:26:26 +0300

    enable search for a movie

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -3,14 +3,42 @@ import './Search.css';
 import SingleContent from '../../components/SingleContent/SingleContent';
 
 function Search() {
-    const [searchQuery, setSearchQuery] = useState('');
+    const [name, setName] = useState('')
+    const [content, setContent] = useState([]);
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    },[])
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=8d07a996f1bf5f9cee356b413d8fa485&query="${name}`)
+        .then((response) => response.json())
+        .then((data) => {setContent(data.results)})
+    }
 
     return(
-        <div className="search">
+        <div className="search" onSubmit={handleSubmit} >
             <form className="search">
-                <input type="text" className="search-input" placeholder="Search..." />
-                <button>Search</button>
+                <input type="text" className="search-input" placeholder="Search..." onChange={(e) => setName(e.target.value)} />
+                <button type='submit' onClick={handleSubmit}>Search</button>
             </form>
+            <div className="trending">
+                {content.map((s) => (
+                    <SingleContent 
+                        key={s._id}
+                        id={s._id}
+                        poster={s.poster_path}
+                        title={s.title || s.name}
+                        date={s.release_date || s.first_air_date}
+                        media_type={s.media_type}
+                        rating={s.vote_average}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
