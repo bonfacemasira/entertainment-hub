c18e69d263b3a99b0c63bfa29bcc49e7be1c7d9c
Bonface Masira <bonfacemasira@gmail.com>
2022-07-31 03:06:54 +0300

    add content details page to view eac selected show

---
+++ src/components/ContentDetails/ContentDetails.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect} from 'react';
+import './ContentDetails.css';
+
+function ContentDetails() {
+    <div>
+        My Movies
+    </div>
+}
+
+export default ContentDetails;
