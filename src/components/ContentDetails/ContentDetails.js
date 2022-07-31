1b4b33326075e583a24387829166d2a9063f8331
Bonface Masira <bonfacemasira@gmail.com>
2022-07-31 23:42:04 +0300

    enable onclick of a movie to send it to favorites

--- src/components/ContentDetails/ContentDetails.js
+++ src/components/ContentDetails/ContentDetails.js
@@ -3,26 +3,11 @@ import { useParams } from 'react-router-dom';
 import './ContentDetails.css';
 
 function ContentDetails({data}) {
-    const [show, setShow] = useState(false);
-    console.log(data);
-
-    function handleOpen() {
-        setShow(true);
-    };
-
-    function handleClose(){
-        setShow(false);
-    }
-
     return(
-        <div>
-            <div onClick={handleOpen}>
-                <div onClick={handleClose}>
-                    {<img src={data.poster_path}/>}
-                </div>
-            </div>
-        </div>
-    )
+    <div className='movieDetails'>
+        {data.overview}
+    </div>
+)
 }
 
 export default ContentDetails;
