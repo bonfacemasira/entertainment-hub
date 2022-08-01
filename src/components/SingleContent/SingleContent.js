2d39f5ec91c99995f4ea14c1f27135a13614f388
Bonface Masira <bonfacemasira@gmail.com>
2022-08-01 11:05:10 +0300

    fix bug to send selected favorites to remote server

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -2,30 +2,42 @@ import React, { useState } from 'react';
 import ContentDetails from '../ContentDetails/ContentDetails';
 import './SingleContent.css';
 import axios from 'axios';
+import { Modal } from 'react-bootstrap';
 
 
 function SingleContent({data, id, poster, title, date, media_type, rating}) {
-    const [isShown, setIsShown] = useState(false);
+    const [showStatus,setShowStatus] =useState(false)
+
+    const handleShow = () => setShowStatus(true)
+    const handleClose = () => setShowStatus(false)
 
     const onSelect = () => {
-        axios.post("http://localhost:4000/results", data);
+        axios.post("https://enternment-hubv1-server.herokuapp.com/results", data);
     };
 
 
     let source = `https://image.tmdb.org/t/p/w1280${poster}`
 
     return (
-        <div className="single-content" key={id} >
-            <img className="image" src={source} alt={title} />
-            <div className="description-container">
-                <p className="title">{title}</p>
-                <div className="holder">
-                    <span>{media_type === "movie" ? "Movie" : "TV Series"}</span>
-                    <span>{date}</span>
+        <div>
+            <div className="single-content" key={id} >
+                <img className="image" src={source} alt={title} />
+                <div className="description-container">
+                    <p className="title">{title}</p>
+                    <div className="holder">
+                        <span>{media_type === "movie" ? "Movie" : "TV Series"}</span>
+                        <span>{date}</span>
+                    </div>
+                    <p className="rating">Rating: {rating}</p>
                 </div>
-                <p className="rating">Rating: {rating}</p>
+                <button onClick={() =>{onSelect(id)}}>Add to Favorite</button>
             </div>
-            <button onClick={() =>{onSelect(id)}}>Add to Favorite</button>
+            <Modal open={showStatus} onClose={handleClose} className="rating-modal">
+                <Modal.Header closeButton></Modal.Header>
+                <Modal.Body>
+                    <ContentDetails data={data} />
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
