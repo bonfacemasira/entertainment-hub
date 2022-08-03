1e9d0b03342123d3a99906cfca4c88e44da5199a
Bonface Masira <bonfacemasira@gmail.com>
2022-08-03 23:48:17 +0300

    add form in the Form component

--- src/pages/Form/Form.js
+++ src/pages/Form/Form.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-function Form(){}
+function Form(){
+    return (
+        <div>
+            <form>
+                <input type="hidden" name="id" value="id" />
+                <input type="hidden" name="name" value="name" />
+                <input type="hidden" name="value" value="value" />
+                <input type="hidden" name="checked" value="checked" />
+            </form>
+        </div>
+    )
+}
 
 export default Form;
